8c96065069009f5069bbb8f8afcc28e6d277338a
Raywcl <2728928100@qq.com>
2019-11-04 07:45:58 +0800

    排序算法：快速排序改进：针对有序数组，基本需要O(n2)，改进使用随机数字作为基准值【1】

---
+++ src/main/java/com/algorithm/learn/sort/quicksort/quickSortInOrderClass.java
@@ -0,0 +1,101 @@
+package com.algorithm.learn.sort.quicksort;
+
+import java.util.Random;
+
+/**
+ * Created by Administrator on 2019/11/3.
+ */
+public class quickSortInOrderClass {
+
+    /**
+     * 总结：
+     * 1.改进快速排序时间：10000个数字排序，array 排序耗时：7ms
+     */
+
+    /**
+     * 【1】
+     * 0.快速排序：
+     * @param array 数组
+     * @param left 起始排序位置
+     * @param right 终止排序位置
+     */
+    public void quickSort(int[] array,int left,int right){
+       if (left>=right)
+           return;
+
+        //获得分割位置
+        int p=partition(array,left,right);
+        quickSort(array,left,p-1);
+        quickSort(array,p+1,right);
+    }
+
+    /**
+     * 0.1 功能：把数组最左侧的元素，转移到恰当位置
+     * @param array
+     * @param left
+     * @param right
+     * @return
+     */
+    public int partition(int[] array,int left,int right){
+
+        //定义：array[left+1,j]<v;array[j+1,i)>v。分为两个部分
+        int j=left,i;  //j=left+1，会导致可能和i同时到达最后一位，而且需要和array[left]交换的情况，array[j+1]报出界的错误
+//        int v=array[left]; //作为标准的元素，左侧第一个
+
+        //随机取出一个[left,right]里面的值，使之和第一个数做交换。目的：使随机选出的元素作为标定
+        int temp2=array[(int)Math.random()*(right-left+1)+left];
+        array[(int)Math.random()*(right-left+1)+left]=array[left];
+        array[left]=temp2;
+        int v=array[left];
+
+        for (i=left+1;i<=right;i++){
+            if (array[i]>v){
+//                i++;不需要操作，因为i默认加1
+            }else if(array[i]<v){
+                int temp=array[i];
+                array[i]=array[j+1];
+                array[j+1]=temp;
+                j++;
+            }
+        }
+
+        int temp=array[left];
+        array[left]=array[j];
+        array[j]=temp;
+        return j;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        //生成测试数组测试
+        int numcount=10000;
+        int maxValue=1000;
+        int[] arr=new int[numcount];
+        for (int i=0;i<numcount;i++){
+            arr[i]=(int)(Math.random()*maxValue); //随机数的范围
+        }
+
+//        int[] arr=new int[]{1,50,81,64,3};
+        System.out.println("array 排序前数组：");
+        for (int i : arr) {
+            System.out.print(i+"\t");
+        }
+
+        System.out.println();
+
+        long startTime=System.currentTimeMillis();
+        new quickSortInOrderClass().quickSort(arr,0,arr.length-1); //快速排序
+        long stopTime=System.currentTimeMillis();
+        System.out.println("array 排序耗时："+(stopTime-startTime)+"ms");
+
+
+        System.out.println("array 排序后数组：");
+        for (int i : arr) {
+            System.out.print(i+"\t");
+        }
+
+    }
+
+}
