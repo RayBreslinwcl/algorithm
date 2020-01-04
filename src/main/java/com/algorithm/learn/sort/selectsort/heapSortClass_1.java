a0ba812b6eb85d2458d42de42c3214da9286bf85
Raywcl <2728928100@qq.com>
2020-01-04 21:49:00 +0800

    堆排序：添加备注

--- src/main/java/com/algorithm/learn/sort/selectsort/heapSortClass_1.java
+++ src/main/java/com/algorithm/learn/sort/selectsort/heapSortClass_1.java
@@ -16,6 +16,8 @@ public class heapSortClass_1 {
         // 循环建堆
         for (int i = 0; i < arrayLength - 1; i++) {
             // 建堆：每一次从data[0]到data[arrayLength-1-i]，构建最大堆
+            // 第1次，从0-》arrayLength - 1
+            // 第2次，从0-》arrayLength - 2
             buildMaxdHeap(data, arrayLength - 1 - i);
             // 交换堆顶和最后一个元素：最后一个元素data[arrayLength-1-i]是最大值
             swap(data, 0, arrayLength - 1 - i);
@@ -24,17 +26,19 @@ public class heapSortClass_1 {
     }
 
     // 对data数组从0到lastIndex建大顶堆
+    // 第1次，从0-》arrayLength - 1
+    // 第2次，从0-》arrayLength - 2
     private static void buildMaxdHeap(int[] data, int lastIndex) {
         // 从lastIndex处节点（最后一个节点）的父节点开始
         for (int i = (lastIndex - 1) / 2; i >= 0; i--) {
             // k保存当前正在判断的节点
             int k = i;
-            // 如果当前k节点的子节点存在
+            // 如果当前k节点的子节点存在，其中k * 2 + 1是左子节点
             while (k * 2 + 1 <= lastIndex) {
                 // k节点的左子节点的索引
                 int biggerIndex = 2 * k + 1;
-                // 如果biggerIndex小于lastIndex，即biggerIndex +1
-                // 代表k节点的右子节点存在
+                // 如果biggerIndex小于lastIndex，即biggerIndex +1 <=lastIndex
+                // 即：代表k节点的右子节点存在
                 if (biggerIndex < lastIndex) {
                     // 如果右子节点的值较大
                     if (data[biggerIndex] - data[biggerIndex + 1] < 0) {
