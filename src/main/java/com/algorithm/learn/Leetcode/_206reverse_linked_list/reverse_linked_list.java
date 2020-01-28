b58b9f87ca6089dfc754cc6d0c5bacfdaf4c5f88
Raywcl <2728928100@qq.com>
2020-01-28 17:07:43 +0800

    链表逆转：递归解法

--- src/main/java/com/algorithm/learn/Leetcode/_206reverse_linked_list/reverse_linked_list.java
+++ src/main/java/com/algorithm/learn/Leetcode/_206reverse_linked_list/reverse_linked_list.java
@@ -35,4 +35,42 @@ public class reverse_linked_list {
         }
         return pre;
     }
+    //递归解法：https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+    /**
+     * 递归解法
+     这题有个很骚气的递归解法，递归解法很不好理解，这里最好配合代码和动画一起理解。
+     递归的两个条件：
+
+     终止条件是当前节点或者下一个节点==null
+     在函数内部，改变节点的指向，也就是 head 的下一个节点指向 head 递归函数那句
+     head.next.next = head
+     很不好理解，其实就是 head 的下一个节点指向head。
+     递归函数中每次返回的 cur 其实只最后一个节点，在递归函数内部，改变的是当前节点的指向。
+     动画演示如下：
+     https://leetcode-cn.com/problems/reverse-linked-list/solution/fan-zhuan-lian-biao-by-leetcode/
+
+
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    /**
+     * 理解：从2个节点链表；3个节点链表；4个节点链表可以理解
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head){
+        if(head==null||head.next==null){
+            return head;
+        }
+        //这里的cur就是最后一个节点
+        ListNode cur=reverseList2(head.next);
+        //如果链表是 1->2->3->4->5，那么此时的cur就是5
+        //而head是4，head的下一个是5，下下一个是空
+        //所以head.next.next 就是5->4
+        head.next.next=head;
+        //防止链表循环，需要将head.next设置为空
+        head.next=null;
+        //每层递归函数都返回cur，也就是最后一个节点
+        return cur;
+    }
 }
