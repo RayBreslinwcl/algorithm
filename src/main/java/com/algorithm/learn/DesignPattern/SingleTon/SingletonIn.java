cb361745d8c72cde4b1983b181b300d473f59910
Raywcl <2728928100@qq.com>
2020-03-10 23:28:51 +0800

    设计模式： 1.单例模式：饿汉、懒汉、内部静态类

---
+++ src/main/java/com/algorithm/learn/DesignPattern/SingleTon/SingletonIn.java
@@ -0,0 +1,35 @@
+package com.algorithm.learn.DesignPattern.SingleTon;
+
+/**
+ * Created by Administrator on 2020/3/10.
+ * 【参考：https://github.com/youlookwhat/DesignPattern/blob/master/app/src/main/java/com/example/jingbin/designpattern/singleton/inclass/SingletonIn.java】
+ *
+ *
+ */
+
+public class SingletonIn {
+
+    /**
+     * Created by jingbin on 2016/10/28.
+     * 内部类[推荐用]
+     * <p>
+     * 这种方式跟饿汉式方式采用的机制类似，但又有不同。
+     * 两者都是采用了类装载的机制来保证初始化实例时只有一个线程。
+     * 不同的地方:
+     * 在饿汉式方式是只要Singleton类被装载就会实例化,
+     * 内部类是在需要实例化时，调用getInstance方法，才会装载SingletonHolder类
+     * <p>
+     * 优点：避免了线程不安全，延迟加载，效率高。
+     */
+
+    private SingletonIn() {
+    }
+
+    private static class SingletonInHodler {
+        private static SingletonIn singletonIn = new SingletonIn();
+    }
+
+    public static SingletonIn getSingletonIn() {
+        return SingletonInHodler.singletonIn;
+    }
+}
