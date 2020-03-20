21fa53d4e29987fc7c1f915c5d69d877cb9aee95
wangchongliang <hbhdwcl_1@126.com>
2020-03-20 12:58:21 +0800

    offer:05两个栈实现队列

---
+++ src/main/java/com/algorithm/learn/Leetcode/_01offer/offer04.java
@@ -0,0 +1,31 @@
+package com.algorithm.learn.Leetcode._01offer;
+
+/**
+ * 原文链接
+ * https://www.nowcoder.com/practice/8a19cbe657394eeaac2f6ea9b0f6fcf6?tpId=13&tqId=11157&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking
+ * <p>
+ * #### 题目
+ * <p>
+ * > 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * <p>
+ * 例如：
+ * 输入前序遍历序列`{1,2,4,7,3,5,6,8}`和中序遍历序列`{4,7,2,1,5,3,8,6}`，则重建二叉树并返回。
+ */
+public class offer04 {
+
+    // Definition for binary tree
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        return null;
+    }
+}
