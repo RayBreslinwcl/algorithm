7a3c8829461dcd5b0f9c23ba9ca65dcaef4f58f5
wangchongliang <hbhdwcl_1@126.com>
2020-03-20 12:47:33 +0800

    offer:05两个栈实现队列

---
+++ src/main/java/com/algorithm/learn/Leetcode/_01offer/offer05.java
@@ -0,0 +1,14 @@
+package com.algorithm.learn.Leetcode._01offer;
+
+/**
+ * #### [题目链接](https://www.nowcoder.com/practice/54275ddae22f475981afa2244dd448c6?tpId=13&tqId=11158&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking)
+ *
+ * > https://www.nowcoder.com/practice/54275ddae22f475981afa2244dd448c6?tpId=13&tqId=11158&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking
+ *
+ * #### 题目
+ *
+ * > 用两个栈来实现一个队列，完成队列的`Push`和`Pop`操作。 队列中的元素为`int`类型。
+ * 【总结：已经做过】
+ */
+public class offer05 {
+}
