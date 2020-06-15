c2dd3f86847da2897f2f79f51ef753eddde897ee
wangchongliang <2728928100@qq.com>
2020-06-15 17:00:07 +0800

    24

--- src/main/java/com/algorithm/learn/Leetcode/_24_swap_nodes_in_pairs/_24_swap_nodes_in_pairs.java
+++ src/main/java/com/algorithm/learn/Leetcode/_24_swap_nodes_in_pairs/_24_swap_nodes_in_pairs.java
@@ -11,7 +11,7 @@ package com.algorithm.learn.Leetcode._24_swap_nodes_in_pairs;
  * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 著作权归领扣网络所 有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class _24_swap_nodes_in_pairs {
 
