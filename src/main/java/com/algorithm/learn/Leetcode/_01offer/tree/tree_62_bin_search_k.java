a4d9ff07e9b978b0d0bf3798486b34fe482733c6
ray <2728928100@qq.com>
2021-09-16 15:33:08 +0800

    树的第K个

--- src/main/java/com/algorithm/learn/Leetcode/_01offer/tree/tree_62_bin_search_k.java
+++ src/main/java/com/algorithm/learn/Leetcode/_01offer/tree/tree_62_bin_search_k.java
@@ -1,5 +1,36 @@
 package com.algorithm.learn.Leetcode._01offer.tree;
 
+//public class TreeNode {
+//    int val = 0;
+//    TreeNode left = null;
+//    TreeNode right = null;
+//
+//    public TreeNode(int val) {
+//        this.val = val;
+//    }
+//}
+
+/**
+ * 参考：https://blog.csdn.net/sun10081/article/details/82805991
+ */
 public class tree_62_bin_search_k {
 
+    int count = 0;
+    TreeNode ans;
+    TreeNode KthNode(TreeNode pRoot, int k){
+        helper(pRoot, k);
+        return ans;
+    }
+
+    public void helper(TreeNode node, int k){
+        if (node==null || count>k) {
+            return;
+        }
+        helper(node.left, k);
+        count++;
+        if (count == k) {
+            ans = node;
+        }
+        helper(node.right, k);
+    }
 }
